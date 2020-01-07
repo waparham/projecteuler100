d7f4ed3ee765237ead903ac2ad37eb9c31aaac08
waparham <59574394+waparham@users.noreply.github.com>
2020-01-07 08:06:52 -0500

    Update euler_problem_1.py

--- euler_problem_1.py
+++ euler_problem_1.py
@@ -1,8 +1,8 @@
+#Find the sum of all the multiples of 3 or 5 below 1000.
 y = 0
 for x in range(1000):
     if x % 3 == 0:
         y = y + x
-        print(str(x))
     elif x % 5 == 0:
         y = y + x
     else:
